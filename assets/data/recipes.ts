3e60fcc1901d0a6df18759912b8e06439f1e13ba
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2025-05-05 12:15:07 -0400

    allows rendering of data on app screen

---
+++ assets/data/recipes.ts
@@ -0,0 +1,9 @@
+const recipes = [
+  {
+    id: 1,
+    name: "Pizza",
+    description: "Very good pizza",
+  },
+];
+
+export default recipes;
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,17 +2,15 @@ import { StyleSheet } from "react-native";
 
 import EditScreenInfo from "@/src/components/EditScreenInfo";
 import { Text, View } from "@/src/components/Themed";
+import recipes from "../../../assets/data/recipes";
+
+const recipe = recipes[0];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text style={styles.title}>{recipe.name}</Text>
+      <Text>{recipe.description}</Text>
     </View>
   );
 }
